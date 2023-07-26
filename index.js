fd1371bd030a027b1c672e941671b40eabc5487f
Jimi Ademola <108378126+swampyd@users.noreply.github.com>
2023-07-26 16:17:35 -0600

    Attempting to add transitions to website

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-createCanvas(400, 400);
-background('green');
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
