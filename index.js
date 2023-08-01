0376036798b3a96a1e25f770a362fe5d42099951
Jimi Ademola <108378126+swampyd@users.noreply.github.com>
2023-07-31 18:02:21 -0600

    Working on animations for First Page

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
+window.addEventListener('load', () => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        console.log(entry);
         if (entry.isIntersecting) {
-            entry.target.classList.add('show');
+            entry.target.classList.add('showLeft');
         } else {
-            entry.target.classList.remove('show');
+            entry.target.classList.remove('showLeft');
         }
+      });
     });
-});
 
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
+    const hiddenElements = document.querySelectorAll('.hiddenLeft');
+    hiddenElements.forEach(el => observer.observe(el));
+  });
+
+window.addEventListener('load', () => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+          entry.target.classList.add('showRight');
+        } else {
+            entry.target.classList.remove('showRight');
+        }
+      });
+    });
+
+    const hiddenElements = document.querySelectorAll('.hiddenRight');
+    hiddenElements.forEach(el => observer.observe(el));
+  });
